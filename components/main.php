62a4f88169e67877164960bc97abc8f55c4c95e5
FriJ <egorik580@gmail.com>
2020-03-30 21:36:55 +0500

    framework.css:ul.style, create info

--- components/main.php
+++ components/main.php
@@ -1,6 +1,10 @@
-<?template('main/slider')?>
-<?template('main/diagnostics')?>
-<?template('main/garant')?>
-<?template('main/spare')?>
+<div class="main">
+	<?template('main/slider')?>
+	<?template('main/diagnostics')?>
+	<?template('main/garant')?>
+	<?template('main/spare')?>	
+	<?template('main/info')?>	
+</div>
+
 
 
